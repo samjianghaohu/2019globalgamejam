15ad13cec75d6ff73bbe0f8283f0861d7ffaed72
Sam Jianghao Hu <jh4828@nyu.edu>
2019-01-25 20:51:50 +0800

    Implemented player movement

---
+++ 2019.01.25_GGJ/Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour
+{// This script determines how camera follows the player.
+
+    public Transform playerTrans;
+    public Vector3 camOffset;
+    public Transform boundaries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camTargetPos = playerTrans.position + camOffset;
+
+
+        transform.position = new Vector3(camTargetPos.x, camTargetPos.y, transform.position.z);
+    }
+}
