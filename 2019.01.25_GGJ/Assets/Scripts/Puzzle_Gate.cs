029ec140ed21616ac6960e355ae8739fa916d37f
Sam Jianghao Hu <jh4828@nyu.edu>
2019-01-26 03:48:51 +0800

    Implemented simple a button and gate puzzle

---
+++ 2019.01.25_GGJ/Assets/Scripts/Puzzle_Gate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle_Gate : MonoBehaviour
+{// This script manages gate opening.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Open()
+    {
+        Destroy(this.gameObject);
+    }
+}
