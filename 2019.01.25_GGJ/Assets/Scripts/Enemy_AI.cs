04ffce4f987a0406445207ebb09380899f96887b
Sam Jianghao Hu <jh4828@nyu.edu>
2019-01-26 00:57:03 +0800

    Implemented simple enemy chasing AI

--- 2019.01.25_GGJ/Assets/Scripts/Enemy_AI.cs
+++ 2019.01.25_GGJ/Assets/Scripts/Enemy_AI.cs
@@ -11,16 +11,21 @@ public class Enemy_AI : MonoBehaviour
 
     public Vector3[] waypoints;
     public float patrolSpd;
+    public LayerMask myLayerMask;
 
     int patrolPhaseNum;
     int patrolTurningBack;
     int currentPhase = 1;
+
+    Transform playerTrans;
     
     // Start is called before the first frame update
     void Start()
     {
         patrolPhaseNum = 2 * (waypoints.Length - 1);
         patrolTurningBack = patrolPhaseNum / 2;
+
+        playerTrans = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -37,12 +42,71 @@ public class Enemy_AI : MonoBehaviour
             {
                 targetPos = waypoints[currentPhase - patrolTurningBack];
             }
+
+            // Flip itself depending on the direction it's going.
+            if (targetPos.x >= transform.localPosition.x)
+            {
+                transform.eulerAngles = new Vector3(transform.rotation.x, 0, transform.rotation.z);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(transform.rotation.x, 180, transform.rotation.z);
+            }
+
+
             transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPos, patrolSpd * Time.deltaTime);
             if (Vector3.Distance(transform.localPosition, targetPos) <= 0.0001f)
             {
                 currentPhase += 1;
                 currentPhase = currentPhase % patrolPhaseNum;
             }
+        }else if (state == CHASE)
+        {
+            // Look at the player
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, (playerTrans.position - transform.position), Mathf.Infinity, myLayerMask);
+            //If enemy is able to see player
+            if (hit != null && hit.collider != null && hit.collider.tag == "Player")
+            {
+                if (hit.distance <= 6f)
+                { // Chase player if player's close by
+                    ChasePlayer();
+                }
+                else
+                {
+                    state = PATROL;
+                }
+
+
+
+
+                //Unless enemy is stunned, it should tay patrolling if player is blocked and enemy can't see it
+            }
         }
     }
+
+    public void Chase()
+    {
+        if (state != CHASE)
+        {
+            state = CHASE;
+        }
+    }
+
+
+    void ChasePlayer()
+    {
+        // Flip itself depending on the direction it's going.
+        if (playerTrans.position.x >= transform.position.x)
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.x, 0, transform.rotation.z);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.x, 180, transform.rotation.z);
+        }
+
+
+        transform.position = Vector3.MoveTowards(transform.position, playerTrans.position, patrolSpd * Time.deltaTime);
+    }
+
 }
---
+++ 2019.01.25_GGJ/Assets/Scripts/Enemy_Detect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Detect : MonoBehaviour
+{ // This script triggers enemy's chasing state.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            transform.parent.GetComponent<Enemy_AI>().Chase();
+        }
+    }
+}
