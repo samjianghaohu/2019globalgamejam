c9fb30353e3cf006c46cccc8de2e2082c33fc90d
Sam Jianghao Hu <jh4828@nyu.edu>
2019-01-27 06:38:53 +0800

    Implemented BGM manager

---
+++ 2019.01.25_GGJ/Assets/Scripts/Audio_BGMManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_BGMManager : MonoBehaviour
+{// This script manages bgm.
+
+    AudioSource myBgmPlayer;
+    public AudioClip[] bgmClips;
+    bool isWaitingf46 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBgmPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWaitingf46)
+        {
+            if (!myBgmPlayer.isPlaying)
+            {
+                myBgmPlayer.clip = bgmClips[6];
+                myBgmPlayer.loop = true;
+                myBgmPlayer.Play();
+                isWaitingf46 = false;
+            }
+        }
+    }
+
+
+    public void PlayClip(int clipNum)
+    {
+        if (clipNum != -1)
+        {
+            if (clipNum == 5)
+            {
+                myBgmPlayer.Stop();
+                myBgmPlayer.clip = bgmClips[5];
+                myBgmPlayer.loop = false;
+                myBgmPlayer.Play();
+                isWaitingf46 = true;
+            }
+            else
+            {
+                myBgmPlayer.Stop();
+                myBgmPlayer.clip = bgmClips[clipNum];
+                myBgmPlayer.loop = true;
+                myBgmPlayer.Play();
+                isWaitingf46 = false;
+            }
+        }
+    }
+}
--- 2019.01.25_GGJ/Assets/Scripts/Portal_Transition.cs
+++ 2019.01.25_GGJ/Assets/Scripts/Portal_Transition.cs
@@ -8,8 +8,10 @@ public class Portal_Transition : MonoBehaviour
     public Transform targetAreaTrans;
     public Transform camTrans;
     public float fadeSpd;
+    public int bgmNum;
 
     GameObject player;
+    GameObject bgmManager;
     bool isTransiting = false;
     bool ifRelocated = false;
 
@@ -17,6 +19,7 @@ public class Portal_Transition : MonoBehaviour
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        bgmManager = GameObject.FindWithTag("BGM");
     }
 
     // Update is called once per frame
@@ -39,6 +42,9 @@ public class Portal_Transition : MonoBehaviour
                     camTrans.GetComponent<Camera_Movement>().boundaries = targetAreaTrans.GetChild(0);
                     Vector3 newPlayerPos = targetAreaTrans.GetChild(1).position;
                     player.transform.position = newPlayerPos;
+
+                    bgmManager.GetComponent<Audio_BGMManager>().PlayClip(bgmNum);
+
                     ifRelocated = true;
                 }
                 else
