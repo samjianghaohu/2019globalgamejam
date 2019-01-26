e805540a59869a5a93444d2ca008019736264040
Sam Jianghao Hu <jh4828@nyu.edu>
2019-01-27 05:06:52 +0800

    Implemented player life points

---
+++ 2019.01.25_GGJ/Assets/Scripts/Player_LifePoints.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_LifePoints : MonoBehaviour
+{// This script manages player taking damage.
+
+    int lifePoint = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifePoint <= 0)
+        {
+            Debug.Log("Game Over");
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemies") { }
+        lifePoint -= 1;
+    }
+}
