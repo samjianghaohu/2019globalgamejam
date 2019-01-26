e2d0d8f93f73d294068260f6bb540d900991be45
Sam Jianghao Hu <jh4828@nyu.edu>
2019-01-26 15:06:08 +0800

    Implemented form switch

---
+++ 2019.01.25_GGJ/Assets/Scripts/Player_AnimationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_AnimationController : MonoBehaviour
+{// This script manages player animation;
+
+    Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void BecomeKid()
+    {
+        myAnimator.SetTrigger("BecomeKid");
+    }
+
+
+    public void BecomeOld()
+    {
+        myAnimator.SetTrigger("BecomeOld");
+    }
+}
---
+++ 2019.01.25_GGJ/Assets/Scripts/Player_FormSwitch.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_FormSwitch : MonoBehaviour
+{// This script manages switching between old and kid form.
+
+    int OLD = 0;
+    int KID = 1;
+    int state = 0;
+
+    public GameObject hudObj;
+
+    GameObject player;
+    Player_AnimationController playerAnim;
+    Player_Movement playerMv;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        playerAnim = player.GetComponent<Player_AnimationController>();
+        playerMv = player.GetComponent<Player_Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            if (state == OLD)
+            {
+                ChangeToKid();
+                state = KID;
+            }
+            else if (state == KID)
+            {
+                ChangeToOld();
+                state = OLD;
+            }
+        }
+    }
+
+    public void ChangeToKid()
+    {
+        playerMv.mvSpd = 350f;
+        playerAnim.BecomeKid();
+
+    }
+
+    public void ChangeToOld()
+    {
+        playerMv.mvSpd = 200f;
+        playerAnim.BecomeOld();
+    }
+}
