7a6e9bdaed5c0fdc65c2a3cabb34967337ec0764
Sam Jianghao Hu <jh4828@nyu.edu>
2019-01-26 23:05:05 +0800

    Implemented security level

---
+++ 2019.01.25_GGJ/Assets/Scripts/Player_SecurityLevel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_SecurityLevel : MonoBehaviour
+{// This script manages the security level.
+
+    public float increaseSpd;
+    public float decreaseSpd;
+    public bool isKid = false;
+    public Slider securitySldr;
+
+    float level = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isKid)
+        {
+            level += increaseSpd * Time.deltaTime;
+            if (level > 1f)
+            {
+                level = 1f;
+            }
+        }
+        else
+        {
+            level -= decreaseSpd * Time.deltaTime;
+            if (level < 0f)
+            {
+                level = 0f;
+            }
+
+        }
+
+
+        DrawLevel();
+    }
+
+
+    void DrawLevel()
+    {
+        securitySldr.value = level;
+        Color sliderColor = Color.Lerp(Color.white, Color.red, level);
+        securitySldr.transform.GetChild(1).GetChild(0).GetComponent<Image>().color = sliderColor;
+    }
+}
