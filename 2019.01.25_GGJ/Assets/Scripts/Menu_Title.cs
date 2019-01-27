76e5d5c07a8ed6a09cf1ee138b1526e5e6ed0565
Sam Jianghao Hu <jh4828@nyu.edu>
2019-01-27 14:17:15 +0800

    Added title menu

---
+++ 2019.01.25_GGJ/Assets/Scripts/Menu_Title.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Title : MonoBehaviour
+{
+    int START = 0;
+    int QUIT = 1;
+    int select = 0;
+
+    public Vector3 iconPos1;
+    public Vector3 iconPos2;
+
+    AudioSource selectSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectSFX = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            select += 1;
+            selectSFX.Play();
+        }
+        PlaceIcon();
+        SelectOpt();
+    }
+
+
+    void PlaceIcon()
+    {
+        if (select % 2 == START)
+        {
+            transform.position = iconPos1;
+        }
+        else
+        {
+            transform.position = iconPos2;
+        }
+    }
+
+
+    void SelectOpt()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.J))
+        {
+            if (select % 2 == START)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
